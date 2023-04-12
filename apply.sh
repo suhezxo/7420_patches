5d4e4ad2d0e8365c107531f7261e26b91e27e489
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-04-12 11:22:59 +0100

    Add apply.sh script
    Automates patching process
    
    extract in home folder to ~/7420_patches-lineage-19.1 else it wont work

---
+++ apply.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cd build/soong
+git am --signoff < ~/7420_patches-lineage-19.1/build_soong/0000-Allow-warnins-from-hardware.patch
+cd ../../
+cd frameworks/base
+git am --signoff < ~/7420_patches-lineage-19.1/frameworks_base/0001-BiometricScheduler-Cancel-operation-if-not-idle.patch
+git am --signoff < ~/7420_patches-lineage-19.1/frameworks_base/0002.patch
+git am --signoff < ~/7420_patches-lineage-19.1/frameworks_base/0003.patch
+cd ../../
+cd frameworks/native
+git am --signoff < ~/7420_patches-lineage-19.1/frameworks_native/0001-Add-back-pre-S-createEventQueue-function.patch
+cd ../../
+cd system/netd
+git am --signoff < ~/7420_patches-lineage-19.1/system_netd/0001-Revert-the-removal-of-non-BPF-network-traffic-monito.patch
+git am --signoff < ~/7420_patches-lineage-19.1/system_netd/0002-netd-Allow-devices-to-force-add-directly-connected-r.patch
+cd ../../
+cd hardware/libhardware
+git am --signoff < ~/7420_patches-lineage-19.1/hardware_libhardware/0001-keystore-hackup.patch
+cd ../../
+cd packages/modules/adb
+git am --signoff < ~/7420_patches-lineage-19.1/packages_modules_adb/0001-adb-Bring-back-support-for-legacy-FunctionFS.patch
+cd ../../../
+cd system/bpf
+git am --signoff < ~/7420_patches-lineage-19.1/system_bpf/0001-Ignore-bpf-errors-for-4.9-kernels.patch
+git am --signoff < ~/7420_patches-lineage-19.1/system_bpf/0002-Bring-back-isBpfSupported-method-but-using-ro.kernel.patch
+cd ../../
+cd system/security
+git am --signoff < ~/7420_patches-lineage-19.1/system_security/0001-keystore-hackup.patch
+cd ../../
+cd vendor/lineage
+git am --signoff < ~/7420_patches-lineage-19.1/vendor_lineage/0001-Disable-adb-secure.patch
+cd ../../
