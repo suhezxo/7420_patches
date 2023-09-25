7347346e3e1aa8ccf69ae22d91d7bb8737df8498
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-09-25 12:39:46 +0100

    DIsable legacy wifi patches
    for now as it seems the non-legacy HAL is stable in day to day use

--- apply.sh
+++ apply.sh
@@ -42,10 +42,10 @@ cd system/core
 git am --signoff < ~/patches-lineage-20.0/system_core/0001-Add-no-BPF-usecase-support.patch
 cd ../../
 cd hardware/lineage/interfaces
-git am --signoff < ~/patches-lineage-20.0/hardware_lineage_interfaces/0001-wifi-1.0-legacy-Add-provision-to-create-remove-dynam.patch
-git am --signoff < ~/patches-lineage-20.0/hardware_lineage_interfaces/0002-wifi-fix-legacy-HIDL-for-T.patch
-git am --signoff < ~/patches-lineage-20.0/hardware_lineage_interfaces/0003-wifi-hidl_struct_util.cpp-convertLegacyWifiChannelWi.patch
-git am --signoff < ~/patches-lineage-20.0/hardware_lineage_interfaces/0004-wifi-wifi.h-fix-build-undef-NAN.patch
+#git am --signoff < ~/patches-lineage-20.0/hardware_lineage_interfaces/0001-wifi-1.0-legacy-Add-provision-to-create-remove-dynam.patch
+#git am --signoff < ~/patches-lineage-20.0/hardware_lineage_interfaces/0002-wifi-fix-legacy-HIDL-for-T.patch
+#git am --signoff < ~/patches-lineage-20.0/hardware_lineage_interfaces/0003-wifi-hidl_struct_util.cpp-convertLegacyWifiChannelWi.patch
+#git am --signoff < ~/patches-lineage-20.0/hardware_lineage_interfaces/0004-wifi-wifi.h-fix-build-undef-NAN.patch
 cd ../../../
 cd vendor/lineage
 git am --signoff < ~/patches-lineage-20.0/vendor_lineage/0000-Disable-adb-secure.patch
