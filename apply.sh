924c26d5dc2456dc77dbfb606da2e1d8d072bb14
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2024-02-15 11:41:37 +0000

    Allow warnings from hardware.
    Our linaro sources currently throw warnings due to gralloc print errors. Ignore them as they don't cause problems.
    
    Eventually fix them but since we are patching anyway  save me some effort and put this here

--- apply.sh
+++ apply.sh
@@ -4,3 +4,6 @@ cd art
 echo "Applying patches to art"
 git am --signoff < ~/patches-lineage-18.1/android_art/0001-Cache-operations-dont-segfault-on-our-kernel.patch
 cd ../
+cd build/soong
+git am --signoff < ~/patches-lineage-18.1/build_soong/0001-Allow-warnings-from-hardware.patch
+cd ../../
