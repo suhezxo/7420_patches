4c715068a41ed3032db8750c0c054e0be2c468e2
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2024-08-09 09:39:31 +0100

    Correct path to patches

--- apply.sh
+++ apply.sh
@@ -2,5 +2,5 @@
 
 cd art
 echo "Applying patches to art"
-git am --signoff < ~/patches-lineage-18.1/android_art/0001-Ignore-checking-for-cache-segfaults-based-on-kernel-.patch
+git am --signoff < ~/7420_patches-lineage-18.1/android_art/0001-Ignore-checking-for-cache-segfaults-based-on-kernel-.patch
 cd ../
