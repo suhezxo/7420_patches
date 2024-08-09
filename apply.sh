0efbbdfa3526a0438b8424df0dc0332a0a7dd63d
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2024-08-09 09:39:05 +0100

    Remove build patches
    This is no longer needed after changes to BSP

--- apply.sh
+++ apply.sh
@@ -4,7 +4,3 @@ cd art
 echo "Applying patches to art"
 git am --signoff < ~/patches-lineage-18.1/android_art/0001-Ignore-checking-for-cache-segfaults-based-on-kernel-.patch
 cd ../
-cd build/soong
-echo "Applying patches to build/soong"
-git am --signoff < ~/patches-lineage-18.1/build_soong/0001-Allow-build-warnings-from-hardware.patch
-cd ../../
